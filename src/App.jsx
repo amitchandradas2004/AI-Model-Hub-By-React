005fdae5c02c4800fb7a0c3c5b395ba9563c9ed0
amitchandradas2004 <amitcdas792@gmail.com>
2026-04-14 23:19:12 +0600

    Footer is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import Banner from "./Banner/Banner";
 import NavBar from "./Components/Navbar/Navbar";
+import Footer from "./Footer/Footer";
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
-
       <Banner></Banner>
+      <Footer></Footer>
     </>
   );
 }
